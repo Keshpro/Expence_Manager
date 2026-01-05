eeb5195863b6c711ac4e0ed2b88ae0f89e82013d
Keshan Panditharathna <keshanebusiness@gmail.com>
2026-01-05 15:28:33 +0530

    updated

--- src/main/java/com/expence/demo/config/CorsConfig.java
+++ src/main/java/com/expence/demo/config/CorsConfig.java
@@ -13,7 +13,6 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
